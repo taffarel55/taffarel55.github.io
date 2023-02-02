5431083f3534b1a252f1a7345249632a5f777ded
taffarel55 <mauricio.silva@db.tec.br>
2023-02-02 14:04:47 -0300

    🚧 working...

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer>Hello, I'm the footer.</footer>
+
+<style>
+	footer {
+		padding: 1rem;
+		background: lightskyblue;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
